64e3bebc9bea962021cd78bef87fc8e433caea57
Aaruph <aaruph12345@gmail.com>
2025-01-21 14:19:08 +0545

    WishlistRoute Added

---
+++ routes/WishlistRoute.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const router = express.Router();
+const { findAll, save, findById, deleteById } = require("../controller/WishlistController");
+
+
+
+router.get("/", findAll);
+router.post("/", save);
+router.get("/:id", findById);
+router.delete("/:id", deleteById);
+
+
+
+
+module.exports = router;
